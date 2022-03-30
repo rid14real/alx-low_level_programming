1ec5e4bbbd3301f682698357f95baf04bc396098
Ridwan Badmus <rid14real@gmail.com>
2022-03-30 01:18:03 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- ** main - Entry Point
+** main - Entry Point
  *
- * Description: 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
  * Return: Always 1 (Required)
  **/
-
 int main(void)
 {
 	write(1, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
