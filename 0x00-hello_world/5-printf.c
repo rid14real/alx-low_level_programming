735627349618805b7895929380fcd1028ea9adb2
Ridwan Badmus <rid14real@gmail.com>
2022-03-30 00:32:59 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entry Point
  *
  * Return: Always 0 (Sucess)
  **/
-
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
