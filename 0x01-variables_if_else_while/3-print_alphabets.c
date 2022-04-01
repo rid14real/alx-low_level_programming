711d54f3d831b0a2e13e7bb6eb286546bd89705a
Ridwan Badmus <62150969+rid14real@users.noreply.github.com>
2022-04-01 20:48:43 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alpabetetic in lower and upper case
+ * main - Prints the alphabets in lower and upper case
  *
  * Return: Always (Success)
  **/
 int main(void)
 {
 		char c;
-		
+
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			putchar(c);
 		}
-		
+
 		for (c = 'A'; c <= 'Z'; c++)
 		{
 			putchar(c);
