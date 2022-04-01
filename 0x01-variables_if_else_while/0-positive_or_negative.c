f13fcf74fbe2eaa5b562127644ee1dee2b9a9be1
Ridwan Badmus <rid14real@gmail.com>
2022-04-01 10:39:47 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
- *  *main- positive, negative or zero
- *   *
+ * main - positive, negative or zero
  *
- *   Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
