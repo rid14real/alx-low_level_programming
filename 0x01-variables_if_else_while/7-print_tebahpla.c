cd0c48cba320bde1b3069cc6185506ddab7ec24b
Ridwan Badmus <62150969+rid14real@users.noreply.github.com>
2022-04-02 18:18:54 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabets at reverse
+ * main - Prints the alphabet at reverse
  *
- * Result: Always (Success)
+ * Return: Always (Success)
  **/
 int main(void)
 {
