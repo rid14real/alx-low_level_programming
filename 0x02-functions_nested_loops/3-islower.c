227a94276d946dc330191309eb28888ce03e95f9
Ridwan Badmus <rid14real@gmail.com>
2022-04-10 10:01:46 -0700

    printing lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ **/
+int _islower(init c)
+{
+	return (c >= 'a' && c <= 'z');
+}
